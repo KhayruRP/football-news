4688162fa3f4ab2337e8450b751a8a066427415a
KhayruRP <khayru.rafamanda@ui.ac.id>
2025-09-10 16:46:34 +0700

    Commit 5 - Tutorial 2

--- football_news/settings.py
+++ football_news/settings.py
@@ -35,7 +35,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "khayru-rafamanda-footballnews.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://khayru-rafamanda-footballnews.pbp.cs.ui.ac.id/"
+    "https://khayru-rafamanda-footballnews.pbp.cs.ui.ac.id"
 ]
 
 
